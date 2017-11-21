2a8118e7f3468238d042b99349d007391c6344bb
KITRI <KITRI@KITRI-PC>
2017-11-21 17:54:28 +0900

    20171121

--- Helloworld/src/reference_pro/GetMean.java
+++ Helloworld/src/reference_pro/GetMean.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 public class GetMean {
 
 	public int getMean(int[] array) {
+		int[] list = new int[array.length];
+		int sum = 0;
+		
         return 0;
     }
 	
 	public int getMax(int[] array) {
-	       /* 구현하기 */
-		return 0;
+//		int[] list = new int[array.length];
+		int[] list = {1,5,3,8,2};
+		int max = 0;
+		for(int i=0; i<array.length; i++) {
+			if(list[i]>max) {
+				max = list[i];
+			}
+		}
+		System.out.println("max:" + max);
+		return max;
 	}
 	
 	public int getMin(int[] array) {
@@ -33,12 +44,16 @@ public class GetMean {
 //		Scanner sc = new Scanner(System.in);
 //        int num = sc.nextInt;
 //        int x[] = new int[num];
-//        GetMean getMean = new GetMean();
+        GetMean getMean = new GetMean();
         // 아래는 배열에 값을 집어넣는 매서드입니다.
 //        x = getNumber();
 
         // 아래는 테스트로 출력해 보기 위한 코드입니다.
 //        System.out.println("평균값 : " + getMean.getMean(x));
+        
+        
+        
+//		System.out.println("최대값: " +getMean.getMax(array));
 
 	}
 
